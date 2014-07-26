205e9708c05e409b5299c3c924f44e9a2b47ab49
Arven Draconis <brian.johnathan.b@gmail.com>
2014-07-26 19:48:42 -0400

    Update getbootstrapimport.user.js
    Some strange handler not being updated.

--- getbootstrapimport.user.js
+++ getbootstrapimport.user.js
@@ -9,34 +9,34 @@
 //   .tld is magic that matches all top-level domains (e.g. .com, .co.uk, .us, etc.)
 
 function setvalues() {
-var testfile = prompt("Please enter the path to your less variables data", "http://github.com/Arven/arven-design/testfile.less");
-if (testfile != null) {
-
-  // Create a request, asking the server for a JSON response via the Accept header
-  // Also logging whenever the state of the request changes
-  var ret = GM_xmlhttpRequest({
-    method: "GET",
-    headers: {"Accept": "application/json"},
-    url: testfile,
-    onreadystatechange: function(res) {
-      GM_log("Request state changed to: " + res.readyState);
-    },
-    onload: function(res) {
-      // Lets assume we get Text back...
-      var text = res.responseText;
-      lines = text.split("\n");
-      for(var i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        if(line.substr(0, 1) == "@") {
-          // We are actually parsing a variable.
-          var m = line.match(/@([\w-]+):\s*(value)/);
-          alert(m[0] + " set value to " + m[1]);
+  var testfile = prompt("Please enter the path to your less variables data", "http://github.com/Arven/arven-design/testfile.less");
+  if (testfile != null) {
+  
+    // Create a request, asking the server for a JSON response via the Accept header
+    // Also logging whenever the state of the request changes
+    var ret = GM_xmlhttpRequest({
+      method: "GET",
+      headers: {"Accept": "application/json"},
+      url: testfile,
+      onreadystatechange: function(res) {
+        GM_log("Request state changed to: " + res.readyState);
+      },
+      onload: function(res) {
+        // Lets assume we get Text back...
+        var text = res.responseText;
+        lines = text.split("\n");
+        for(var i = 0; i < lines.length; i++) {
+          var line = lines[i];
+          if(line.substr(0, 1) == "@") {
+            // We are actually parsing a variable.
+            var m = line.match(/@([\w-]+):\s*(value)/);
+            alert(m[0] + " set value to " + m[1]);
+          }
         }
       }
-    }
-  });
-
-}
+    });
+  
+  }
 }
 
 GM_registerMenuCommand( "Set values for Bootstrap", setvalues );
