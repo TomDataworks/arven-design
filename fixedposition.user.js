188c4a94cf77570d209f6a8469577e0e6b27cc0a
Arven Draconis <brian.johnathan.b@gmail.com>
2014-08-01 21:20:36 -0500

    Update fixedposition.user.js

--- fixedposition.user.js
+++ fixedposition.user.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name          FixedPositionToStatic
+// @namespace     http://github.com/Arven/arven-design
+// @description	  Converts fixed positioned elements to static
+// @include       http://*.tld/*
+// ==/UserScript==
+// Notes:
+//   * is a wildcard character
+//   .tld is magic that matches all top-level domains (e.g. .com, .co.uk, .us, etc.)
+
 Array.forEach(
     document.querySelectorAll("div")
     ,function(el) {
